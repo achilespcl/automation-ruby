8d265b64abdec93c21d6e5d35e75afd342e5d20c
achilespcl <achilespcluciano@gmail.com>
2019-02-06 23:46:24 -0300

    basic structure

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,50 +1,6 @@
 require 'capybara'
-require 'capybara/dsl'
-require 'capybara/rspec/matchers'
-require 'selenium-webdriver'
-require 'site_prism'
-require 'rspec'
-require_relative 'page_helper.rb'
-require_relative 'helper.rb'
-
-#brunao
-World(Capybara::DSL)
-World(Capybara::RSpecMatchers)
-World(Pages)
-
-HEADLESS = ENV['HEADLESS']
-ENVIRONMENT_TYPE = ENV['ENVIRONMENT_TYPE']
-
-CONFIG = YAML.load_file(File.dirname(__FILE__) +
-"/data/#{ENVIRONMENT_TYPE}.yml")
-
-
-Capybara.register_driver :selenium do |app|
-
-if HEADLESS.eql?('sem_headless')
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      'chromeOptions' => { 'args' => ['--disable-infobars',
-                                      'window-size=1600,1024'] }
-    )
-  )
-elsif HEADLESS.eql?('com_headless')
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      'chromeOptions' => { 'args' => ['headless', 'disable-gpu',
-                                      '--disable-infobars',
-                                      'window-size=1600,1024'] }
-    )
-  )
-end
-end
+require 'capybara/cucumber'
 
 Capybara.configure do |config|
-    config.default_driver = :selenium
-    config.default_max_wait_time = 10
-    config.app_host = CONFIG['url_home']
+  config.default_driver = :selenium_chrome
 end
